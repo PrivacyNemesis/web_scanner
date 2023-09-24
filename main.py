dd1c1486c36c5c7aeaecbd5bd130d17bdd1ae0fa
Sleyter Monteiro <87656234+sleyter-monteiro@users.noreply.github.com>
2023-09-24 09:31:25 +0200

    update main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# coding:utf-8
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import requests
 import mechanize
+import random
 
 def my_ip():
     session = HTMLSession()
@@ -10,9 +12,11 @@ def my_ip():
     soup = BeautifulSoup(response.content, 'html.parser')
     ip = soup.find('span', {'id': 'ip4'}).text
     print(f"Votre adresse IP est {ip}")
-
+    
+my_ip()
+    
 def get_proxy():
-    free_proxy_url = "https://free-proxy-list.net/anonymous-proxy.html"
+    free_proxy_url = "https://free-proxy-list.net/"
     response = requests.get(free_proxy_url)
 
     if response.status_code == 200:
@@ -30,16 +34,41 @@ def get_proxy():
                 https = columns[6].text
                 last_checked = columns[7].text
                 
-                if https == "yes" and last_checked <= "30 secs ago" or last_checked == "1 min ago":
+                if https == "yes" and last_checked <= "30 secs ago":
                     print(f"IP: {ip}, Port: {port}, Pays: {country}, Actualisation: {last_checked}")
         else:
             print("Aucun proxy disponible.")
     else:
         print("Echec de la requête vers free-proxy-list.net")
-
-my_ip()
-get_proxy()        
+    return ip, port           
 
 
+def target(url,):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    user_agent = [("User-agent", "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0")]
+    browser.addheaders = user_agent
+    url_list = []
+    ip, port = random.choice(get_proxy())[:2]
+    browser.set_proxies({"https": f"{ip}:{port}"})
+    if url.endswith("/"):
+        url = url.rstrip("/")
+    page = browser.open(url)
+    soup = BeautifulSoup(page, "html.parser")
+    for link in soup.find_all("a"):
+        l = link.get("href")
+        if len(l) > 0 and not l.startswith("#"):
+            if l.startswith("/"):
+                l = url + l
+            if l not in url_list:
+                url_list.append(l)
+    for link in url_list:
+        print(link)
+    # for cookie in browser.cookiejar:
+    #     print(cookie)
+    # print(page.read())
+
+
+target("https://cyberini.com")
 
 
